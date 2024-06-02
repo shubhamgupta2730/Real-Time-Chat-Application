bb9aca81b50fd31268b3329b7b5963c65d3cd508
Shubham Gupta <shubhamgupta2730@gmail.com>
2024-06-02 14:29:31 +0530

    socket event handling and add client-side event listener

--- public/script.js
+++ public/script.js
@@ -1,1 +1,12 @@
 var socket = io();
+
+let btn = document.getElementById('btn');
+btn.onclick = function exec(){
+  socket.emit('from_client');
+}
+console.log('collected a new event from server');
+socket.on('from_server', ()=>{
+  const div = document.createElement('div');
+  div.innerText = 'New event from server';
+  document.body.appendChild(div);
+})
