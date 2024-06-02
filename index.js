4007db627a25cd5ee01a37b4a6c7fa5d84ca225d
Shubham Gupta <shubhamgupta2730@gmail.com>
2024-06-02 11:26:05 +0530

    connected with io object

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const http = require('http');
-const socketio  = require('socket.io');
+const socketio = require('socket.io');
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 app.use('/', express.static(__dirname + '/public'));
 
+io.on('connection', (socket) => {
+  console.log('a user connected', socket.id);
+});
 
-server.listen(3000,()=>{
+
+
+
+server.listen(3000, () => {
   console.log('server started');
 });
